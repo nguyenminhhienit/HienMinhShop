6f3fb29ce367f6711c314411ace7089c8a1b424e
HIENNM\hienminh <nguyenminhhienit@gmail.com>
2018-05-21 21:14:40 +0700

    bai10

--- HienMinhShop.Data/Infrastructure/IRepository.cs
+++ HienMinhShop.Data/Infrastructure/IRepository.cs
@@ -26,10 +26,9 @@ namespace HienMinhShop.Data.Infrastructure
         T GetSingleById(int id);
 
         T GetSingleByCondition(Expression<Func<T, bool>> expression, string[] includes = null);
-
-        IQueryable<T> GetAll(string[] includes = null);
-        IQueryable<T> GetMulti(Expression<Func<T, bool>> expression, string[] includes = null);
-        IQueryable<T> GetMultiPaging(Expression<Func<T, bool>> filter, out int total, int index = 0, int size = 50, string[] includes = null);
+        IEnumerable<T> GetAll(string[] includes = null);
+        IEnumerable<T> GetMulti(Expression<Func<T, bool>> expression, string[] includes = null);
+        IEnumerable<T> GetMultiPaging(Expression<Func<T, bool>> filter, out int total, int index = 0, int size = 50, string[] includes = null);
         int Count(Expression<Func<T, bool>> where);
         bool CheckContains(Expression<Func<T, bool>> predicate);
     }
--- HienMinhShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
+++ HienMinhShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using HienMinhShop.Data.Infrastructure;
+using HienMinhShop.Data.Repositories;
+using HienMinhShop.Model.Models;
+using HienMinhShop.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +15,56 @@ namespace HienMinhShop.UnitTest.ServiceTest
     [TestClass]
     public class PostCategoryServiceTest
     {
+        private Mock<IPostCategoryRepository> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private IPostCategoryService _categoryService;
+        private List<PostCategory> _listCategory;
+
         [TestInitialize]
         public void Initialize()
         {
-
+            _mockRepository = new Mock<IPostCategoryRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _categoryService = new PostCategoryService(_mockRepository.Object, _mockUnitOfWork.Object);
+            _listCategory = new List<PostCategory>()
+            {
+                new PostCategory(){ID=1, Name ="DM1" },
+                new PostCategory(){ID=2, Name ="DM2" },
+                new PostCategory(){ID=3, Name ="DM3" },
+            };
         }
 
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            //setup method
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(_listCategory);
+
+            //call action
+            var result = _categoryService.GetAll() as List<PostCategory>;
+
+            //compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test";
+            category.Alias = "test";
+
+            _mockRepository.Setup(m => m.Add(category)).Returns((PostCategory p) =>
+              {
+                  p.ID = 1;
+                  return p;
+              });
+
+            var result = _categoryService.Add(category);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ID);
+        }
     }
 }
